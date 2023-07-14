4f468c67698157745319792b4fbbcf0db21eb311
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2023-07-15 00:13:50 +0900

    Create TupleToObject.ts

---
+++ easy/TupleToObject.ts
@@ -0,0 +1,7 @@
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00011-easy-tuple-to-object/README.md
+
+type TupleToObject<T extends readonly (string | number)[]> = { [K in T[number]]: K }
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type result = TupleToObject<typeof tuple> // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
