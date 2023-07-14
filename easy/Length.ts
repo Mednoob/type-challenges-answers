a011abbb3eb98ef0ee68922ec84785b44db82719
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2023-07-15 00:35:44 +0900

    Create Length.ts

---
+++ easy/Length.ts
@@ -0,0 +1,9 @@
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00018-easy-tuple-length/README.md
+
+type Length<T extends readonly any[]> = T["length"]
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+type teslaLength = Length<tesla>  // expected 4
+type spaceXLength = Length<spaceX> // expected 5
