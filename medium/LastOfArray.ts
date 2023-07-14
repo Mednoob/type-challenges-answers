ff725a0e4321c40e1f96a60aa3ac0246ca8a9b40
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2023-07-15 01:56:49 +0900

    Create LastOfArray.ts

---
+++ medium/LastOfArray.ts
@@ -0,0 +1,8 @@
+type IsSingle<T extends any[]> = T extends [any, any, ...any[]] ? false : true
+type Last<T extends any[]> = IsSingle<T> extends true ? T[0] : T extends [any, ...infer A] ? Last<A> : never
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type tail1 = Last<arr1> // expected to be 'c'
+type tail2 = Last<arr2> // expected to be 1
