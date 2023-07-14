2bf6ddf9def73dddf7d7f778c26721657c4b9347
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2023-07-15 01:09:08 +0900

    Create ReturnType.ts

---
+++ medium/ReturnType.ts
@@ -0,0 +1,12 @@
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00002-medium-return-type/README.md
+
+type MyReturnType<T> = T extends ((...args: any[]) => infer R) ? R : never
+
+const fn = (v: boolean) => {
+  if (v)
+    return 1
+  else
+    return 2
+}
+
+type a = MyReturnType<typeof fn> // should be "1 | 2"
