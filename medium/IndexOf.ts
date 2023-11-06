9cdfb7a2f9b7807f99a2d8c4f1e1304fc3155c48
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2023-11-06 10:39:36 +0900

    Update IndexOf.ts

--- medium/IndexOf.ts
+++ medium/IndexOf.ts
@@ -1,5 +1,6 @@
 // https://github.com/type-challenges/type-challenges/blob/main/questions/05153-medium-indexof/README.md
 
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
 type IndexOf<T, U extends string | number, I extends any[] = []> =
   T extends [infer L extends string | number, ...infer R]
     ? Equal<L, U> extends true
